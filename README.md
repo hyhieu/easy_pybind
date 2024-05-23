5e9637a5a32df5111e79c62ca8ffaec173f727d6
hyhieu <hyhieu@gmail.com>
2024-05-23 08:01:50 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Easy PyBind11
---
+++ main.py
@@ -0,0 +1,9 @@
+"""Entry point to generate PyBind11 bindings."""
+
+
+def main(unused_args: list[str] | None = None) -> None:
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
